f958696d56e3aab1557b8a2e18f0aaef390ff6ea
SleepingAlbaricoque <tnqls0421@naver.com>
2023-03-14 01:05:19 +0900

    2023.03.13 sts 프로젝트에 admin, cs 화면구현 파일 삽입

--- swiftER/src/main/java/kr/co/swiftER/controller/AdminController.java
+++ swiftER/src/main/java/kr/co/swiftER/controller/AdminController.java
@@ -1,8 +1,84 @@
 package kr.co.swiftER.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import kr.co.swiftER.service.AdminService;
 
 @Controller
 public class AdminController {
 
+	@Autowired
+	private AdminService service;
+	
+	@GetMapping("admin/main")
+	public String main() {
+		return "admin/admin_main";
+	}
+	@GetMapping("admin/member")
+	public String member() {
+		return "admin/admin_member";
+	}
+	
+	@GetMapping("admin/memberView")
+	public String memberView() {
+		return "admin/admin_member_view";
+	}
+	
+	@GetMapping("admin/cs/noticeList")
+	public String noticeList() {
+		return "admin/admin_cs_notice_list";
+	}
+	
+	@GetMapping("admin/cs/noticeView")
+	public String noticeView() {
+		return "admin/admin_cs_notice_view";
+	}
+	
+	@GetMapping("admin/cs/noticeWrite")
+	public String noticeWrite() {
+		return "admin/admin_cs_notice_write";
+	}
+	
+	@GetMapping("admin/cs/noticeModify")
+	public String noticeModify() {
+		return "admin/admin_cs_notice_modify";
+	}
+	
+	@GetMapping("admin/cs/faqList")
+	public String faqList() {
+		return "admin/admin_cs_faq_list";
+	}
+	
+	@GetMapping("admin/cs/faqView")
+	public String faqView() {
+		return "admin/admin_cs_faq_view";
+	}
+	
+	@GetMapping("admin/cs/faqWrite")
+	public String faqWrite() {
+		return "admin/admin_cs_faq_write";
+	}
+	
+	@GetMapping("admin/cs/faqModify")
+	public String faqModify() {
+		return "admin/admin_cs_faq_modify";
+	}
+	
+	@GetMapping("admin/cs/qnaList")
+	public String qnaList() {
+		return "admin/admin_cs_qna_list";
+	}
+	
+	@GetMapping("admin/cs/qnaView")
+	public String qnaView() {
+		return "admin/admin_cs_qna_view";
+	}
+	
+	@GetMapping("admin/cs/qnaWrite")
+	public String qnaWrite() {
+		return "admin/admin_cs_qna_write";
+	}
+	
 }
--- swiftER/src/main/java/kr/co/swiftER/controller/CSController.java
+++ swiftER/src/main/java/kr/co/swiftER/controller/CSController.java
@@ -1,8 +1,49 @@
 package kr.co.swiftER.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import kr.co.swiftER.service.CSService;
 
 @Controller
 public class CSController {
 
+	@Autowired
+	private CSService service;
+	
+	@GetMapping("cs/index")
+	public String index() {
+		return "cs/index";
+	}
+	
+	@GetMapping("cs/notice")
+	public String notice() {
+		return "cs/notice";
+	}
+	
+	@GetMapping("cs/noticeView")
+	public String noticeView() {
+		return "cs/notice_view";
+	}
+	
+	@GetMapping("cs/faq")
+	public String faq() {
+		return "cs/faq";
+	}
+	
+	@GetMapping("cs/qna")
+	public String qna() {
+		return "cs/qna";
+	}
+	
+	@GetMapping("cs/qnaView")
+	public String qnaView() {
+		return "qna_view";
+	}
+	
+	@GetMapping("cs/qnaWrite")
+	public String qna_write() {
+		return "cs/qna_write";
+	}
 }
