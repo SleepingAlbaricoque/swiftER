44af85cf2f2233aca341061008339021f234d4c2
SleepingAlbaricoque <tnqls0421@naver.com>
2023-03-23 13:58:38 +0900

    2023.03.23 admin member 완료, member view 작업 중

--- swiftER/src/main/java/kr/co/swiftER/service/AdminService.java
+++ swiftER/src/main/java/kr/co/swiftER/service/AdminService.java
@@ -15,10 +15,78 @@ public class AdminService {
 	@Autowired
 	private AdminDAO dao;
 	
-	public List<AdminMemberSearchVO> selectMembers(int start){
-		return dao.selectMembers(start);
+	// 전체 회원의 수
+	public List<AdminMemberSearchVO> selectMembers(int start, int isDoc){
+		return dao.selectMembers(start, isDoc);
+	}
+	
+	// 인증 승인 요청한 의사의 수
+	public int selectDocCountTotal() {
+		return dao.selectDocCountTotal();
+	}
+	
+	// 회원 정보 조회(한 명)
+	public AdminMemberSearchVO selectMember(String uid) {
+		return dao.selectMember(uid);
+	}
+	
+	// 회원 영구 정지
+	public int banMember(String uid) {
+		if(dao.banMember(uid) == 1)
+			return 1;
+		
+		return 0;
 	}
 	
 	
+	// 페이징
+	// 글 총 갯수(total)
+	public int selectCountTotal() {
+		return dao.selectCountTotal();
+	}
 	
+	// 현재 페이지 버튼 번호
+	public int getCurrentPage(String pg) {
+		int currentPage = 1;
+		
+		if(pg != null)
+			currentPage = Integer.parseInt(pg);
+		
+		return currentPage;
+	}
+	
+	// 현재 페이지 버튼의 페이지(article) 시작값
+	public int getLimitStart(int currentPage, int articlesPerPage) {
+		return (currentPage - 1) * articlesPerPage;
+	}
+	
+	// 마지막 버튼 페이지 번호
+	public int getLastPageNum(int total, int articlesPerPage) {
+		int lastPageNum = 0;
+		
+		if(total % articlesPerPage == 0)
+			lastPageNum = total /articlesPerPage;
+		else
+			lastPageNum = total / articlesPerPage + 1;
+		
+		return lastPageNum;
+	}
+	
+	// 페이지 시작 번호
+	public int getPageStartNum(int total, int start) {
+		return total - start;
+	}
+	
+	// 페이지 그룹
+	public int[] getPageGroup(int currentPage, int lastPageNum, int articlesPerPage) {
+		int groupCurrent = (int) Math.ceil(currentPage/Double.valueOf(articlesPerPage)); // 현재 페이지 번호 그룹
+		int groupStart = (groupCurrent - 1)*articlesPerPage + 1; // 현재 그룹의 시작 버튼 번호
+		int groupEnd = groupCurrent * articlesPerPage; // 현재 그룹의 마지막 버튼 번호
+		
+		if(groupEnd > lastPageNum)
+			groupEnd = lastPageNum;
+		
+		int[] groups = {groupStart, groupEnd};
+		return groups;
+	}
 }
