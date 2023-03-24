db98d6326c146870afeb5314f6b19c4bb4fc542d
alswns3906 <alswns3906@naver.com>
2023-03-24 13:42:12 +0900

    2023-03-24 erDetail 리뷰 화면 구현 완료

---
+++ swiftER/src/main/resources/static/js/popup.js
@@ -0,0 +1,27 @@
+$(function(){
+    // 상품평 작성 팝업 띄우기
+    $('.reviewbtn').click(function(e){
+        e.preventDefault();
+        $('#popReview').addClass('on');
+    });
+            
+    // 팝업 닫기
+    $('.btnClose').click(function(){                
+        $(this).closest('.popup').removeClass('on');                
+    });
+
+    // 상품평 작성 레이팅바 기능
+    $(".my-rating").starRating({
+        starSize: 20,
+        useFullStars: true,
+        strokeWidth: 0,
+        useGradient: false,
+        minRating: 1,
+        ratedColors: ['#ffa400', '#ffa400', '#ffa400', '#ffa400', '#ffa400'],
+        callback: function(currentRating, $el){
+            alert('rated ' + currentRating);
+            console.log('DOM element ', $el);
+        }
+    });
+
+});
