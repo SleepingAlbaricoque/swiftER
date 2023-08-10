d40e5afb4b39a4358994ab45938127110876a484
SleepingAlbaricoque <tnqls0421@naver.com>
2023-08-10 20:51:50 +0900

    2023.08.10 message 대화 목록 로드 순서 최신순으로 설정

--- swiftER/src/main/java/kr/co/swiftER/controller/MessageController.java
+++ swiftER/src/main/java/kr/co/swiftER/controller/MessageController.java
@@ -3,9 +3,12 @@ package kr.co.swiftER.controller;
 import java.security.Principal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
@@ -45,20 +48,20 @@ public class MessageController {
 		// 현재 로그인한 사용자 정보 가져오기
 		String username = principal.getName();
 		
-		//messages 테이블에서 현재 로그인한 사용자가 보냈거나 받은 메세지 모두 받아오기
+		//messages 테이블에서 현재 로그인한 사용자가 보냈거나 받은 메시지 모두 받아오기
 		List<MessageEntity> messages = repo.findByReceiverOrSender(username, username);
 		
-		// 현재 사용자와 타 사용자 간 마지막 메세지 정보를 저장하기 위한 map 객체 생성
+		// 현재 사용자와 타 사용자 간 마지막 메시지 정보를 저장하기 위한 map 객체 생성
 		Map<String, MessageEntity> lastMessageMap = new HashMap<>();
 		
 		for(MessageEntity message : messages) {
 			// 사용자의 대화 상대 정보를 담기 위한 객체
 			String otherUser;
 			
-			// 메세지의 송신자가 현재 사용자와 같을 경우 메세지의 수신자를 otherUser로 지정
+			// 메시지의 송신자가 현재 사용자와 같을 경우 메시지의 수신자를 otherUser로 지정
 			if(message.getSender().equals(username)) {
 				otherUser = message.getReceiver();
-			// 메세지의 수신자가 현재 사용자와 같을 경우 메세지의 송신자를 otherUser로 지정
+			// 메시지의 수신자가 현재 사용자와 같을 경우 메시지의 송신자를 otherUser로 지정
 			}else {
 				otherUser = message.getSender();
 			}
@@ -66,13 +69,22 @@ public class MessageController {
 			// 이미 해당 otherUser가 map 객체에 들어가있는 지 확인
 			MessageEntity lastMessage = lastMessageMap.get(otherUser);
 			
-			// 해당 otherUser이 map 객체에 아직 추가되기 전이거나, 추가되어있는 메세지가 현재 for loop의 메세지보다 오래된 경우 현재 for loop의 otherUser 및 메세지로 다시 map 객체에 넣기
+			// 해당 otherUser이 map 객체에 아직 추가되기 전이거나, 추가되어있는 메시지가 현재 for loop의 메시지보다 오래된 경우 현재 for loop의 otherUser 및 메시지로 다시 map 객체에 넣기
 			if(lastMessage == null || message.getRdate().compareTo(lastMessage.getRdate()) >0) {
 				lastMessageMap.put(otherUser, message);
 			}
 		}
 		
-		model.addAttribute("lastMessages", lastMessageMap);
+		// lastMessageMap의 메시지를 최신순으로 정렬 => 대화 목록에서 대화탭을 최신순으로 정렬해서 보여 줄 수 있음
+		List<Entry<String, MessageEntity>> sortedMsgList = new ArrayList<>(lastMessageMap.entrySet()); 
+		sortedMsgList.sort(Entry.<String, MessageEntity>comparingByValue(Comparator.comparing(value -> value.getRdate())).reversed()); // lastMessageMap의 Entry를 rdate에 따라 desc 정렬
+		
+		Map<String, MessageEntity> sortedMsgMap = new LinkedHashMap<>(); // 넣는 순서대로 값을 꺼내기 위해서는 LinkedHashMap 필요
+		for(Entry<String, MessageEntity> entry : sortedMsgList) // 정렬된 entry 값을 순서대로 LinkedHashMap 객체에 넣기
+			sortedMsgMap.put(entry.getKey(), entry.getValue());
+		
+		
+		model.addAttribute("lastMessages", sortedMsgMap);
 		model.addAttribute("currentUser", username);
 		return "messages/message";
 	}
@@ -80,12 +92,12 @@ public class MessageController {
 	@ResponseBody
 	@GetMapping("/conversation/{username}")
 	public List<MessageEntity> getConversation(Principal principal, @PathVariable String username) {
-		// 현재 사용자와 username을 가진 유저와의 메세지 가져오기
+		// 현재 사용자와 username을 가진 유저와의 메시지 가져오기
 		List<MessageEntity> messages = repo.findByReceiverAndSenderOrSenderAndReceiverOrderByRdateAsc(principal.getName(), username, principal.getName(), username);
 		
-		// 읽지 않은 메세지를 읽음으로 표시(DB에 isRead값을 0에서 1로 바꾸기)
+		// 읽지 않은 메시지를 읽음으로 표시(DB에 isRead값을 0에서 1로 바꾸기)
 		for(MessageEntity message : messages) {
-			if(message.getIsRead() ==0 && message.getReceiver().equals(principal.getName())) { // isRead값이 0이고, 수신인이 현재 로그인된 유저인 메세지만 선택
+			if(message.getIsRead() ==0 && message.getReceiver().equals(principal.getName())) { // isRead값이 0이고, 수신인이 현재 로그인된 유저인 메시지만 선택
 				message.setIsRead(1);
 				repo.save(message);
 			}
